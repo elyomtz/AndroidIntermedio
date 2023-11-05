264308513197e4fef1262c23d068cd901b68990d
elyo-mr <elyo_mr@outlook.com>
2023-11-05 16:36:24 -0600

    Adapters an view holders added

--- app/src/main/java/com/elyomr/horoscapp/ui/horoscope/HoroscopeViewModel.kt
+++ app/src/main/java/com/elyomr/horoscapp/ui/horoscope/HoroscopeViewModel.kt
@@ -1,9 +1,24 @@
 package com.elyomr.horoscapp.ui.horoscope
 
 import androidx.lifecycle.ViewModel
+import com.elyomr.horoscapp.data.providers.HoroscopeProvider
+import com.elyomr.horoscapp.domain.model.HoroscopeInfo
+import com.elyomr.horoscapp.domain.model.HoroscopeInfo.*
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class HoroscopeViewModel @Inject constructor():ViewModel(){
+class HoroscopeViewModel @Inject constructor(horoscopeProvider: HoroscopeProvider):ViewModel(){
+
+    private var _horoscope = MutableStateFlow<List<HoroscopeInfo>>(emptyList())
+    val horoscope:StateFlow<List<HoroscopeInfo>> = _horoscope
+
+    init{
+        _horoscope.value = horoscopeProvider.getHoroscopes()
+    }
+
+
+
 }
---
+++ app/src/main/java/com/elyomr/horoscapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
@@ -0,0 +1,17 @@
+package com.elyomr.horoscapp.ui.horoscope.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.elyomr.horoscapp.databinding.ItemHoroscopeBinding
+import com.elyomr.horoscapp.domain.model.HoroscopeInfo
+
+class HoroscopeViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+    private val binding = ItemHoroscopeBinding.bind(view)
+
+    fun render(horoscopeInfo: HoroscopeInfo){
+        val context = binding.tvTitle.context
+        binding.ivHoroscope.setImageResource(horoscopeInfo.img)
+        binding.tvTitle.text = context.getString(horoscopeInfo.name)
+    }
+}
