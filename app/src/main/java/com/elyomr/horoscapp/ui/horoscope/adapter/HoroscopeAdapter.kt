58f42289fc6ca9913f252caab58e248d428857ca
elyo-mr <elyo_mr@outlook.com>
2023-11-05 19:55:24 -0600

    Detail and viewmodel added, themes added

--- app/src/main/java/com/elyomr/horoscapp/ui/horoscope/adapter/HoroscopeAdapter.kt
+++ app/src/main/java/com/elyomr/horoscapp/ui/horoscope/adapter/HoroscopeAdapter.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.elyomr.horoscapp.R
 import com.elyomr.horoscapp.domain.model.HoroscopeInfo
 
-class HoroscopeAdapter(private var horoscopeList: List<HoroscopeInfo> = emptyList()) :
+class HoroscopeAdapter(private var horoscopeList: List<HoroscopeInfo> = emptyList(),private val onItemSelected:(HoroscopeInfo)->Unit) :
     RecyclerView.Adapter<HoroscopeViewHolder>() {
 
     fun updateList(list:List<HoroscopeInfo>){
@@ -24,6 +24,6 @@ class HoroscopeAdapter(private var horoscopeList: List<HoroscopeInfo> = emptyLis
     override fun getItemCount()=horoscopeList.size
 
     override fun onBindViewHolder(holder: HoroscopeViewHolder, position: Int) {
-        holder.render(horoscopeList[position])
+        holder.render(horoscopeList[position], onItemSelected)
     }
 }
